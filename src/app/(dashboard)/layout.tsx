62e35f97f3fa437dc3d12426e4dd8995492633af
sgbateria-spec <sgbateria@gmail.com>
2025-11-10 09:23:15 -0300

    Creamos nuestros componentes comúnes de nuestra página dashboard/layout.tsx, el logo con el nombre, el menu, la barra de buscar y los iconos de usuario, creamos nuestros componentes components/Menu.tsx y components/Navbar.tsx

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,13 +10,17 @@ export default function DashboardLayout({
   }>) {
     return <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
         <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
           <Image src="/drums.jpg" alt="logo" width={32} height={32} />
           <span className="hidden lg:block">Drums-SG</span>
         </Link>
+        <Menu/>
       </div>
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">r</div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <Navbar/>
+        {children}  
+      </div>
     </div>;   
   }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,89 +1,86 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const menuItems = [
   {
     title: "MENU",
     items: [
       {
         icon: "/home.png",
-        label: "Home",
+        label: "Inicio",
         href: "/",
         visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/teacher.png",
-        label: "Teachers",
-        href: "/list/teachers",
+        label: "Maestros",
+        href: "/list/maestros",
         visible: ["admin", "teacher"],
       },
       {
         icon: "/student.png",
-        label: "Students",
-        href: "/list/students",
-        visible: ["admin", "teacher"],
-      },
-      {
-        icon: "/parent.png",
-        label: "Parents",
-        href: "/list/parents",
+        label: "Alumnos",
+        href: "/list/alumnos",
         visible: ["admin", "teacher"],
       },
       {
         icon: "/subject.png",
-        label: "Subjects",
-        href: "/list/subjects",
+        label: "Materias",
+        href: "/list/materias",
         visible: ["admin"],
       },
       {
         icon: "/class.png",
-        label: "Classes",
-        href: "/list/classes",
+        label: "Clases",
+        href: "/list/clases",
         visible: ["admin", "teacher"],
       },
       {
         icon: "/lesson.png",
-        label: "Lessons",
-        href: "/list/lessons",
+        label: "Lecciones",
+        href: "/list/lecciones",
         visible: ["admin", "teacher"],
       },
       {
         icon: "/exam.png",
-        label: "Exams",
-        href: "/list/exams",
+        label: "Examenes",
+        href: "/list/examenes",
         visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/assignment.png",
-        label: "Assignments",
-        href: "/list/assignments",
+        label: "Asignaciones",
+        href: "/list/asignaciones",
         visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/result.png",
-        label: "Results",
-        href: "/list/results",
+        label: "Resultados",
+        href: "/list/resultados",
         visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/attendance.png",
-        label: "Attendance",
-        href: "/list/attendance",
+        label: "Asistencia",
+        href: "/list/asistencia",
         visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/calendar.png",
-        label: "Events",
-        href: "/list/events",
+        label: "Eventos",
+        href: "/list/eventos",
         visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/message.png",
-        label: "Messages",
-        href: "/list/messages",
+        label: "Mensages",
+        href: "/list/mensages",
         visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/announcement.png",
-        label: "Announcements",
-        href: "/list/announcements",
+        label: "Anuncios",
+        href: "/list/anuncios",
         visible: ["admin", "teacher", "student", "parent"],
       },
     ],
@@ -93,22 +90,42 @@ const menuItems = [
     items: [
       {
         icon: "/profile.png",
-        label: "Profile",
-        href: "/profile",
+        label: "Perfil",
+        href: "/perfil",
         visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/setting.png",
-        label: "Settings",
-        href: "/settings",
+        label: "Configuación",
+        href: "/configuración",
         visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/logout.png",
-        label: "Logout",
-        href: "/logout",
+        label: "Salir",
+        href: "/salir",
         visible: ["admin", "teacher", "student", "parent"],
       },
     ],
   },
 ];
+
+const Menu = () => {
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map(i => (
+        <div className="flex flex-col gap-2" key={i.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
+          {i.items.map(item => (
+            <Link href={item.href} key={item.label} className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
+              <Image src={item.icon} alt="" width={20} height={20} />
+              <span className="hidden lg:block">{item.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Menu
