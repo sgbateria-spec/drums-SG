c724d90595576101977fc09635fcfa9d4cb996cd
sgbateria-spec <sgbateria@gmail.com>
2025-11-11 09:22:33 -0300

    Continuamos creando la página de admin/page.tsx, agregamos colores al archivo tailwind.config.ts, creamos un nuevo componente en components/UserCard.tsx

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,21 @@
+import UserCard from "@/components/UserCard"
+
 const AdminPage = () => {
     return (
-        <div className="">AdminPage</div>
+        <div className="p-4 flex  gap-4 flex-col md:flex-row">
+            {/* LEFT */}
+            <div className="w-full lg:w-2/3">
+                {/* USER CARDS */}
+                <div className="flex gap-4 justify-between flex-wrap">
+                    <UserCard type="alumnos"/>
+                    <UserCard type="maestros"/>
+                    <UserCard type="lecciones"/>
+                    <UserCard type="canciones"/>
+                </div>
+            </div>
+            {/* RIGHT */}
+            <div className="w-full lg:w-1/3">r</div>
+        </div>
     )
 }
 
