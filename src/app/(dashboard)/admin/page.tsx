33c0df1ac834f5077323c7b1108270b51f51fe15
sgbateria-spec <sgbateria@gmail.com>
2025-11-13 10:13:56 -0300

    Creamos la sección de Anuncios en la parte derecha de nuestra página dashboard/admin/page.tsx, para esto creamos el componente components/Announcements.tsx, también cambiamos la letra de nuestro logo, DRUMS-SG en el archivo dashboard/layout.tsx

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import Announcements from "@/components/Annoucements"
 import AttendanceChart from "@/components/AttendanceChart"
 import CountChart from "@/components/CountChart"
 import EventCalendar from "@/components/EventCalendar"
@@ -34,8 +35,8 @@ const AdminPage = () => {
             </div>
             {/* RIGHT */}
             <div className="w-full lg:w-1/3 flex flex-col gap-8">
-                <EventCalendar />
-
+                <EventCalendar/>
+                <Announcements/>
             </div>
         </div>
     )
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -13,7 +13,7 @@ export default function DashboardLayout({
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
         <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
           <Image src="/drums.jpg" alt="logo" width={32} height={32} />
-          <span className="hidden lg:block">Drums-SG</span>
+          <span className="hidden lg:block font-bold">Drums-SG</span>
         </Link>
         <Menu/>
       </div>
