a1e5b5d9941971b3ba8a4d7e34c122fe2062741b
sgbateria-spec <sgbateria@gmail.com>
2025-11-13 09:15:14 -0300

    Instalamos react-calendar, creamos la sección derecha de nuestra página dashboard/admin/page.tsx, la cual tiene un calendario y eventos, para esto creamos el archivo components/EventCAledar.tsx, luego le dimos estilos css en el archivo src/app/globals.css

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from "@/components/AttendanceChart"
 import CountChart from "@/components/CountChart"
+import EventCalendar from "@/components/EventCalendar"
 import FinanceChart from "@/components/FinanceChart"
 import UserCard from "@/components/UserCard"
 
@@ -10,29 +11,32 @@ const AdminPage = () => {
             <div className="w-full lg:w-2/3 flex flex-col gap-8">
                 {/* USER CARDS */}
                 <div className="flex gap-4 justify-between flex-wrap">
-                    <UserCard type="alumnos"/>
-                    <UserCard type="maestros"/>
-                    <UserCard type="lecciones"/>
-                    <UserCard type="canciones"/>
+                    <UserCard type="alumnos" />
+                    <UserCard type="maestros" />
+                    <UserCard type="lecciones" />
+                    <UserCard type="canciones" />
                 </div>
                 {/* MIDDLE CHARTS */}
                 <div className="flex gap-4 flex-col lg:flex-row">
-                {/* COUNT CHARTS */}
-                <div className="w-full lg:w-1/3 h-[450px]">
-                    <CountChart/>
-                </div>
-                {/* ATTENDANCE CHARTS */}
-                <div className="w-full lg:w-2/3 h-[450px]">
-                    <AttendanceChart/>
-                </div>
+                    {/* COUNT CHARTS */}
+                    <div className="w-full lg:w-1/3 h-[450px]">
+                        <CountChart />
+                    </div>
+                    {/* ATTENDANCE CHARTS */}
+                    <div className="w-full lg:w-2/3 h-[450px]">
+                        <AttendanceChart />
+                    </div>
                 </div>
                 {/* BOTTOM CHARTS */}
                 <div className="w-full h-[500px]">
-                    <FinanceChart/>
+                    <FinanceChart />
                 </div>
             </div>
             {/* RIGHT */}
-            <div className="w-full lg:w-1/3">r</div>
+            <div className="w-full lg:w-1/3 flex flex-col gap-8">
+                <EventCalendar />
+
+            </div>
         </div>
     )
 }
